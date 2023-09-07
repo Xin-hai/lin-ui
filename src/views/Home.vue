cffd805b2e7ba21a37ee300617d9e8f90da99e31
Xin-hai <aa1940358283@163.com>
2023-09-07 16:51:41 +0800

    初始化 Lin UI 项目

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>lin Ui</h1>
+      <h2>一个厉害的UI框架</h2>
+      <p class="actions">
+        <a href="#">GitHub</a>
+        <a href="#">开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: ""
+}
+</script>
+
+<style scoped lang="scss">
+  .topnav{
+    border: 1px solid red;
+  }
+</style>
