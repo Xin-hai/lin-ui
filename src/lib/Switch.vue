a460aeb9cd6685a2745b457f7a0f1022f09908d3
Xin-hai <aa1940358283@163.com>
2023-09-09 00:16:18 +0800

    完成 切换功能

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,14 +1,22 @@
 <template>
-  <button><span></span></button>
+  <button :class="{checked: checked}" @click="checked=!checked"><span></span></button>
 </template>
 
-<script>
+<script lang="ts">
+import {ref} from 'vue';
+
 export default {
-  name: ""
+  setup(){
+    const checked = ref(false)
+    return {
+      checked
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+  $background-normal: #bebebe;
   $background-button: #409eff;
   $background-span: #fff;
   $height-button: 22px;
@@ -18,7 +26,7 @@ export default {
     border: none;
     height: $height-button;
     width: 2*$height-button;
-    background: $background-button;
+    background: $background-normal;
     border-radius: $height-button / 2;
   }
     span {
@@ -31,7 +39,10 @@ export default {
       background:$background-span;
       border-radius: $height-span / 2;
   }
-  button:hover > span {
+  button.checked {
+    background: $background-button;
+  }
+  button.checked > span {
     left: calc(100% - #{$height-span} - 2px);
   }
 </style>
