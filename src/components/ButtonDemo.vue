bd7c2eb110c66e47b51555558c915b85435eb114
Xin-hai <aa1940358283@163.com>
2023-09-09 18:15:05 +0800

    完成 Button基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,19 +2,23 @@
   <div>Button示例</div>
   <h1>示例1</h1>
   <div>
-    <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="small">你好呀</Button>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
 
 <script lang="ts">
 import Button from "../lib/Button.vue";
 export default {
+
   components:{
     Button
   },
   setup(){
     const onClick = ()=> {
-      console.log('here')
+
     }
     return {onClick}
   }
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,24 +1,53 @@
 <template>
-  <div :size="size">
-    <button v-bind="rest">
+    <button class="lin-button" :class=" `lin-theme-${theme}` ">
       <slot/>
     </button>
-  </div>
 </template>
 
 <script>
 export default {
-  inheritAttrs: false,
-  setup(props, context){
-    const {size,...rest} = context.attrs
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  },
+  setup(){
 
-    return {size, rest}
+    return {}
   }
 }
 </script>
 
 <style scoped lang="scss">
-div {
-  border: 1px solid red;
-}
+  *{box-sizing: border-box}
+  .lin-button{
+    $height-button: 32px;
+    $color-border: #d9d9d9;
+    $color-font: #333;
+    $color-blue: #40a9ff;
+    $border-radius: 4px;
+
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    height: $height-button;
+    padding: 0 12px;
+    cursor: pointer;
+    white-space: nowrap;
+    background: #fff;
+    color: $color-font;
+    border: 1px solid $color-border;
+    border-radius: $border-radius;
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+    & + & {
+      margin-left: 8px;
+    }
+    &:hover,
+    &:focus {
+      color: $color-blue;
+      border-color: $color-blue;
+      outline: none;
+    }
+  }
 </style>
