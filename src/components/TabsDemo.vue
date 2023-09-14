99d69204c2bb5b2538d125da8058330793cc1a33
Xin-hai <aa1940358283@163.com>
2023-09-15 06:41:09 +0800

    展示内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Tabs>
+    <Tabs v-model:selected="currentTitle">
       <Tab title="导航一">内容一</Tab>
       <Tab title="导航二">内容二</Tab>
     </Tabs>
@@ -10,8 +10,15 @@
 <script lang="ts">
 import Tab from '../lib/Tab.vue';
 import Tabs from '../lib/Tabs.vue'
+import {ref} from 'vue';
 export default {
-  components: {Tabs, Tab}
+  components: {Tabs, Tab},
+  setup(){
+    const currentTitle = ref('导航一')
+    return {
+      currentTitle
+    }
+  }
 }
 </script>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    <div v-for="(title,index) in titles" :key="index" v-text="title"></div>
-    <component v-for="(item,index) in defaults" :is="item" :key="index"></component>
+  <div class="lin-tabs">
+    <div class="lin-tabs-nav">
+      <div v-for="(title,index) in titles" :key="index" v-text="title"  @click="select(title)"
+           :ref="el => {if(el) navItems[index]=el}"
+           class="lin-tabs-nav-item" :class="{'selected': selected===title}"></div>
+      <div class="lin-tabs-nav-line"></div>
+    </div>
+   <div class="lin-tabs-content">
+     <component v-for="(item,index) in defaults" :is="item" :key="index"
+                class="lin-tabs-content-item" :class="{'selected': selected===item.props.title}" />
+   </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from "./Tab.vue";
+import {onMounted, ref} from 'vue';
 export default {
   name: 'Tabs',
+  props: {
+    selected : {
+      type: String
+    }
+  },
   setup(props, context) {
+    const navItems = ref([])
+    onMounted(()=> {
+      // console.log({...navItems.value})
+    })
     const defaults = context.slots.default()
     defaults.forEach((tag)=> {
     // @ts-expect-error
@@ -18,11 +36,63 @@ export default {
       }
     })
     const titles = defaults.map((tag)=>tag.props.title)
-    return {defaults, titles}
+    const select = (title: string)=> {
+      context.emit('update:selected',title)
+    }
+    return {
+      defaults,
+      titles,
+      select,
+      navItems
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+$color-blue: #40a9ff;
+$font-color: #333;
+$border-color: #d9d9d9;
+  .lin-tabs{
+    &-nav {
+      display: flex;
+      position: relative;
+      color: $font-color;
+      border-bottom: 1px solid $border-color;
+
+      &-item {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+
+        &:first-child {
+          margin-left: 0;
+        }
+
+        &.selected {
+          color: $color-blue;
+        }
+      }
+      &-line {
+        height: 3px;
+        width: 100px;
+        position: absolute;
+        left: 0;
+        bottom: -1px;
+        background: $color-blue;
+      }
+    }
+    &-content {
+      padding:  8px 0;
+      ::v-deep &-item {
+        display: none;
+        &.selected {
+          display: block;
+        }
+      }
+    }
+  }
+
+
 
 </style>
