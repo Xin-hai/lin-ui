6e278fe4d306bd86f76de4549f95cc2610980e8e
Xin-hai <aa1940358283@163.com>
2023-09-15 23:18:05 +0800

    抽离相关代码

---
+++ src/components/Switch1.demo.vue
@@ -0,0 +1,16 @@
+<template>
+  <Switch v-model:value="boolean" />
+</template>
+
+<script lang="ts">
+import Switch from "../lib/Switch.vue";
+import {ref} from "vue";
+
+export default {
+  components: {Switch},
+  setup(){
+    const boolean = ref(false)
+    return { boolean }
+  }
+}
+</script>
---
+++ src/components/Switch2.demo.vue
@@ -0,0 +1,12 @@
+<template>
+  <Switch disabled/>
+</template>
+
+<script lang="ts">
+import Switch from "../lib/Switch.vue";
+
+
+export default {
+  components: {Switch}
+}
+</script>
